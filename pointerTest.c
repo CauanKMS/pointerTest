eccce29559ae569cbcc6531686dd1fbb1534d4ec
CauanKMS <cauan.kms@gmail.com>
2018-04-10 19:21:26 -0300

    Added some improvements to pointerTest.c

--- pointerTest.c
+++ pointerTest.c
@@ -5,25 +5,42 @@ int main(){
 	int i;
 	char *point1;
 	char nice = 'n';
-	char v[4][8] = {"green", "yellow", "red", "blue"};	
+	char v[4][8] = {"green", "cyan", "red", "blue"};	
 	char * pointzero[] = {"Gorillaz", "Guns N Roses", "Gamma Ray"};
 
 	point1 = &v[1][0];
 
 	printf("%c\t", *point1);
-	printf("%p\t", point1);
+	printf("%p\n", point1);
+
+	point1 = point1 + 2;
+
+	printf("%c\t", *point1);
+	printf("%p\n", point1);
+	
+	point1 = &v[3][2];
+
+	printf("%c\t", *point1);
+	printf("%p\n", point1);
+
+	point1 = &v[1][2];
+
+	printf("%c\t", *point1);
+	printf("%p\n", point1);
 
 	point1++;
 
 	printf("%c\t", *point1);
-	printf("%p\t", point1);
+	printf("%p\n\n", point1);	
+
+	printf("%s\t %s\t %s\t", &pointzero[0][0], &pointzero[1][0], &pointzero[2][0]);
 	
+	/*
 	point1 = &nice;
 
-	printf("%c\t", *point1);
-	printf("%p\t", point1);
+	printf("%s\t", *point1);
+	printf("%p\n", point1);
 
-	/*
 	printf("%c", pointzero[0]);
 	printf("%p", point1);
 
